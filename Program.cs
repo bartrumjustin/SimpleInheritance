329fc4cc911e69a182d3397e1beb163387d13c21
bartrumjustin <bartrumjustin@gmail.com>
2026-01-30 10:25:27 -0500

    Task 2 derived from task 1 but with 2nd class inherit

--- Program.cs
+++ Program.cs
@@ -51,12 +51,43 @@
             this.isAlive = isAlive;
         }
 
-        public void print()
+        public void printHuman()
         {
             Console.WriteLine("Name:" + "\n" + name + "\n" + "Hair Color:" + "\n" + hairColor + "\n" + "Eye Color:" + "\n" + eyeColor + "\n" + "Still living?:" + "\n" + isAlive + "\n\n");
         }
     }
 
+    class Cat : Animal
+    {
+        public int age;
+        public string owner;
+        public string likes;
+
+        //default constructor
+        public Cat()
+            : base()
+        {
+            age = 0;
+            owner = "";
+            likes = "";
+        }
+
+        //paramatized constructor
+        public Cat(string name, int age, string owner, string likes)
+            : base(name)
+        {
+            this.age = age;
+            this.owner = owner;
+            this.likes = likes;
+        }
+
+
+        //method
+        public void printCat()
+        {
+            Console.WriteLine("Name:" + "\n" + name + "\n" + "Age:" + "\n" + age + "\n" + "Owner:" + "\n" + owner + "\n" + "Prefers:" + "\n" + likes + "\n\n");
+        }
+    }
 
     class Program
     {
@@ -73,11 +104,23 @@
             person.hairColor = "Brown";
             person.eyeColor = "Hazel";
             person.isAlive = true;
-            person.print();
+            person.printHuman();
 
             //derived class object using parameterized constructor
             Human person2 = new Human("Holly", "Blonde", "Blue", true);
-            person2.print();
+            person2.printHuman();
+
+            //derived class object with default second child
+            Cat lucy = new Cat();
+            lucy.name = "Lucy";
+            lucy.age = 3;
+            lucy.owner = "Holly";
+            lucy.likes = "Holly";
+            lucy.printCat();
+            //derived class object with paramatized second child
+            Cat harley = new Cat("Harley", 7, "Justin", "Holly");
+            harley.printCat();
+
         }
 
     }
